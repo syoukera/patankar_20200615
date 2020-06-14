d78e514391064de33d80309ad8ca038632ea9b3c
syoukera <s.akira2986@gmail.com>
2020-06-15 08:35:31 +0900

    Add project files.

---
+++ patankar_20200615/patankar_20200615.f90
@@ -0,0 +1,25 @@
+!  patankar_20200615.f90 
+!
+!  FUNCTIONS:
+!  patankar_20200615 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: patankar_20200615
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program patankar_20200615
+
+    implicit none
+
+    ! Variables
+
+    ! Body of patankar_20200615
+    print *, 'Hello World'
+
+    end program patankar_20200615
+
